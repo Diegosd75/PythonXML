08adb702b7e4c822df5740a8a4451e8919c9a397
Will <w.atuesta@gmail.com>
2024-04-30 17:06:38 -0500

    Se actualiza script adicionando la generacion de un archivo excel con toda la información extraida

--- src/pythonxml.py
+++ src/pythonxml.py
@@ -1,8 +1,11 @@
 import os
 import xml.etree.ElementTree as ET
+import pandas as pd
+
+datafinal=[]
 
 for root, dirs, files in os.walk('C:/REPOSITORIOS/PythonXML'):
-    #print(root)
+    #print(root)-
     if 'DataSource' in root:
         full_path = os.path.join(root)
         if 'v_20230331' in full_path:
@@ -21,6 +24,7 @@ for root, dirs, files in os.walk('C:/REPOSITORIOS/PythonXML'):
                         nombreDS = root_xml.find(".//property[@name='name']").get('value')
                         if nombreDS :
                             print(f"Nombre encontrado: {nombreDS}")
+                            dataconsol ={'nombreDS':nombreDS,'nombreCampo':'','tipoDeDato':'','longitud':''}
                         else:
                             print("Etiqueta <property> encontrada, pero falta información en los atributos.")
 
@@ -33,12 +37,25 @@ for root, dirs, files in os.walk('C:/REPOSITORIOS/PythonXML'):
                                 if tipo_campo == "VARCHAR":
                                     size_property = obj.find(".//property[@name='size']")
                                     size_campo = size_property.get('value')
+                                    dataconsol=dataconsol.copy()
+                                    dataconsol['nombreCampo'] = nombre_campo
+                                    dataconsol['tipoDeDato'] = tipo_campo
+                                    dataconsol['longitud'] = size_campo
+                                    datafinal.append(dataconsol)
                                     print(f"Nombre campo {nombre_campo} y el tipo {tipo_campo} y el size es {size_campo}")
                                 else:
+                                    dataconsol=dataconsol.copy()
+                                    dataconsol['nombreCampo'] = nombre_campo
+                                    dataconsol['tipoDeDato'] = tipo_campo
+                                    datafinal.append(dataconsol)
                                     print(f"Nombre campo {nombre_campo} y el tipo {tipo_campo}")
                                 
                     
                     except ET.ParseError as e:
                         print(f"Error al procesar el archivo {file}: {e}")
 
-            print("#############################################################")
+# Creamos un Dataframe con los datos obtenidos
+df = pd.DataFrame(datafinal)
+
+# Generamos la salida en un informe de tipo excel
+df.to_excel('C:/REPOSITORIOS/PythonXML/salidaxmlcv.xlsx')
